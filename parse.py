c0868f556b0d2830a42248c163d9ed86c3fa6e8f
metalcyanide <chaithanyampcrl@gmail.com>
2021-03-05 01:41:17 +0530

    modified the structure of df

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import os
+import os, re
+import string 
 
 path = '/home/gainyny/Downloads/data_assn1/data_assn1/Train/Source'
 files = []
@@ -18,7 +19,7 @@ for f in files:
         content = "".join(content)
         bs_content = bs(content, "xml")
 
-    df_cols = ["ID", "Sense ID", "head", "context_left", "context_right"]
+    df_cols = ["context_left", "context_right", "head", "Sense ID"]
     rows = []
 
     instances = bs_content.findAll('instance')
@@ -27,10 +28,11 @@ for f in files:
         ID = inst.get('id')
         sense_id = inst.find('answer').get('senseid')
         context_left, head_tag, context_right = inst.find('context').contents
+        context_left = re.split(r'\W+', context_left)
+        context_right = re.split(r'\W+', context_right)
         head = head_tag.text
-        rows.append({"ID": ID, "Sense ID": sense_id, "head": head,
-                    "context_left":context_left , "context_right":context_right})
+        rows.append({"Sense ID": sense_id, "head": head,
+                    "context_left": " ".join(context_left).split()[-2:] , "context_right": " ".join(context_right).split()[:2]})
 
     out_df = pd.DataFrame(rows, columns = df_cols)
-    print(out_df)
 
